06f9646adb62f18dd32530508354d5f8a7b93796
Rob Bertram <rbertram8@gmail.com>
2023-10-15 02:49:44 -0400

    first commit

---
+++ init.lua
@@ -0,0 +1,76 @@
+require('leap').add_default_mappings()
+local config = {
+  colorscheme = "onedark_vivid",
+  plugins = {
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000 -- Ensure it loads first
+  },
+   {"declancm/cinnamon.nvim",
+     event = { "BufRead", "BufNewFile" },
+     config = function() require('cinnamon').setup() end,
+   },
+   {"ggandor/leap.nvim"},
+   {"stevearc/oil.nvim",
+     config = function() require('oil').setup() end
+   },
+   -- Neorg
+   {"nvim-neorg/neorg",
+     run = ":Neorg sync-parsers",
+     config = function()
+       require("neorg").setup {
+         load = {
+           ["core.defaults"] = {},
+           ["core.concealer"] = {},
+           ["core.dirman"] = {
+             config = {
+               workspaces = {
+                 work = "~/notes/work",
+                 kast = "~/notes/kast",
+                 home = "~/notes/home",
+               },
+               default_workspace = "work"
+             }
+           }
+        }
+       }
+     end,
+   },
+   -- blamer (like gitlense)
+   {"APZelos/blamer.nvim"},
+   -- rust tools
+   {"simrat39/rust-tools.nvim"},
+   -- Elm
+ {"ElmCast/elm-vim"},
+  {"vim-test/vim-test"},
+  {"rescript-lang/vim-rescript"},
+  {"evanleck/vim-svelte"},
+  {"itchyny/lightline.vim"},
+  {"MunifTanjim/prettier.nvim"},
+  {"sindrets/diffview.nvim",
+    requires = { { 'nvim-lua/plenary.nvim' } },
+    --    config = function() require("config.flutter").setup() end,
+  },
+  ["tamton-aquib/duck.nvim"] = {
+    config = function()
+      vim.keymap.set('n', '<leader>dd', function() require("duck").hatch() end, {})
+      vim.keymap.set('n', '<leader>dk', function() require("duck").cook() end, {})
+      vim.keymap.set('n', '<leader>dc', function() require("duck").hatch("🐈", 0.75) end, {})
+      vim.keymap.set('n', '<leader>dt', function() require("duck").hatch("🦖", 0.75) end, {})
+    end,
+  },
+
+  },
+  mappings = {
+    n = {
+   ["<S-l>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["<S-h>"] = {
+    function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+    desc = "Previous buffer",
+  }
+    }
+  }
+  -- colorscheme = "poimandres",
+}
+
+return config
